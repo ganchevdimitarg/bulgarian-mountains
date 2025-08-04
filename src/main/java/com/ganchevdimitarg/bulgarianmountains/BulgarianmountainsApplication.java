c3e95c597f0fc743d9752b783f1ccfd5b0c8bf2c
DimitarGanchev <ganchevdimitarg@gmail.com>
2025-08-04 10:38:08 +0300

    start of the project

---
+++ src/main/java/com/ganchevdimitarg/bulgarianmountains/BulgarianmountainsApplication.java
@@ -0,0 +1,13 @@
+package com.ganchevdimitarg.bulgarianmountains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulgarianmountainsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulgarianmountainsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ganchevdimitarg/bulgarianmountains/BulgarianmountainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganchevdimitarg.bulgarianmountains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulgarianmountainsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
